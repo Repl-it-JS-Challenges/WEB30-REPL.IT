30af9f58df3543ffb67af37d4bf45a3bbe30dcf2
Mel Maki <melody.rackham@gmail.com>
2020-04-13 16:41:55 -0600

    Reverse String

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function longestString(arr) {
   let longest = '';
   for (let i = 0; i < arr.length; i++) {
     const string = arr[i];
-    if (string.length > longest.lenth) {
+    if (string.length > longest.length) {
       longest = string;
     }
   }
@@ -13,3 +13,15 @@ function longestString(arr) {
 
 const strings1 = ['short', 'really, really long!', 'medium'];
 console.log(longestString(strings1)); // <--- 'really, really long!'
+
+// #2 Write a function called reverseString that accepts a string and returns a reversed copy of the string.
+
+function reverseString(str) {
+  let seperate = str.split('');
+  let reverse = seperate.reverse();
+  newString = reverse.join('');
+  return newString;
+}
+
+console.log(reverseString('raf'));
+console.log(reverseString('lem'));
