ab297ebe58148a27e9e4b0f0eea2956af6cc4b8e
Mel Maki <melody.rackham@gmail.com>
2020-04-15 14:08:44 -0600

    js1 problem completed

--- js1.js
+++ js1.js
@@ -1,6 +1,5 @@
 // #8 JS -I
 
-console.log('********** Exercise 1 of 4 **********');
 const unimaginativeArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // Write a "for" loop that console.log()'s the "zeroeth"
 // value in the unimaginitiveArray, and thereafter every
@@ -9,14 +8,18 @@ const unimaginativeArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // What is "zeroeth"? Has the word "first" become
 // ambiguous now that I am a computer scientist? (Hint: yes).
 
-console.log('\n********** Exercise 2 of 4 **********');
+for (i = 0; i < unimaginativeArray.length; i += 3) {
+  console.log(unimaginativeArray[i]);
+}
+
 const nameArray = ['Jacquelynn', 'Csaba', 'Ellen', 'Moises', 'Cole', 'Jeff', "Dre'Sean"];
 // Write a console.log() that displays the last value in
 // nameArray. Use the `.length` property of array's
 // to access the last value. What's going on with that
 // slash in Dre'Sean's name?
 
-console.log('\n********** Exercise 3 of 4 **********');
+console.log(nameArray[nameArray.length - 1]);
+
 const adjectiveArray = ['awesome', 'fantastic', 'amazing', 'wonderful', 'fabulous', 'incredible', 'marvelous'];
 // Using both nameArray and adjectiveArray, make a
 // "for" loop that console.log()'s a sentence for each
@@ -25,7 +28,14 @@ const adjectiveArray = ['awesome', 'fantastic', 'amazing', 'wonderful', 'fabulou
 // "Jacquelynn is awesome"
 // "Csaba is fantastic" et cetera...
 
-console.log('\n********** Exercise 4 of 4 **********');
+for (let i = 0; i < adjectiveArray.length; i++) {
+  console.log(`${nameArray[i]} is ${adjectiveArray[i]}`);
+}
+
 // Write a "for" loop that console.log()'s every third
 // number in unimaginativeArray but initialize the
 // iterator so it doesn't console.log() the zero.
+
+for (let i = 3; i < unimaginativeArray.length; i += 3) {
+  console.log(unimaginativeArray[i]);
+}
