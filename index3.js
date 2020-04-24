6523a67c21d5a6affb966238c004439ac0fdeb28
Mel Maki <melody.rackham@gmail.com>
2020-04-24 11:18:47 -0600

    Vowel Count

--- index3.js
+++ index3.js
@@ -1,3 +1,5 @@
+// stack class  - instructions in repl.it
+
 class Stack {
   constructor() {
     this.storage = [];
@@ -29,3 +31,21 @@ console.log(myStack.storage); // <--- [ 'first' ]
 myStack.remove();
 console.log(myStack.storage); // <--- []
 console.log(myStack.numOfItems()); // <--- "There are no items in your Stack."
+
+// vowel count --> Write a function which counts the number of vowels in a given string. Return the count number.
+
+// Passing the string "Hello world!" as an argument to your vowelCount() function would result in the number 3 being returned.
+
+function vowelCount(str) {
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  let lowerCaseSplitStr = str.toLowerCase().split('');
+  let count = 0;
+  lowerCaseSplitStr.forEach((letter) => {
+    if (vowels.includes(letter) === true) {
+      count++;
+    }
+  });
+  return count;
+}
+
+console.log(vowelCount('Hello World!'));
